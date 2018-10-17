29164f76a658aa42c8de21077af132ff12b7be21
xb52 <xb52@cornell.edu>
2018-10-17 00:00:37 -0400

    add string and changed layout

--- JustJava/app/src/main/java/com/example/xinyibabs/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/xinyibabs/justjava/MainActivity.java
@@ -40,7 +40,9 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        displayPrice( quantity * 5);
+       String priceMessage = "Free";
+       displayMessage(priceMessage);
+
     }
 
     /**
@@ -57,6 +59,14 @@ public class MainActivity extends AppCompatActivity {
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 
 }
